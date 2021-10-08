8a4817272d101825496119566a1907470e50c206
yuriiVoikov <yurii.voikov.ua@gmail.com>
2021-10-08 08:17:00 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+@NgModule({
+  imports: [BrowserModule, CommonModule, FormsModule],
+  declarations: [AppComponent, LoginFormComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
